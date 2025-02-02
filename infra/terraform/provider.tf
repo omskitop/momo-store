46520826661cf7af5626f0e798c45ba90a9ecb52
omskitop <omskitop@domob>
2025-02-02 18:34:51 +0300

    fix gitlab tf deploy

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 provider "yandex" {
-  cloud_id  = var.cloud_id # Set your cloud ID
-  folder_id = var.folder_id # Set your cloud folder ID
-  zone      = var.zone # Availability zone by default, one of ru-central1-a, ru-central1-b, ru-central1-c
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
 }
