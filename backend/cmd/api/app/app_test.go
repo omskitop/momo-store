3951555bc9f754ce0cee98265f09fe104cb8255f
omskitop <omskitop@ya.ru>
2025-02-22 17:09:01 +0300

    fix test

--- backend/cmd/api/app/app_test.go
+++ backend/cmd/api/app/app_test.go
@@ -41,21 +41,123 @@ func TestFakeAppIntegrational(t *testing.T) {
 	})
 
 	t.Run("list_dumplings", func(t *testing.T) {
+		// Arrange
 		r := httptest.NewRequest("GET", "/packs", nil)
 		w := httptest.NewRecorder()
+	
+		// Act
 		app.ListDumplingsController(w, r)
-
+	
+		// Assert
 		assert.Equal(t, http.StatusOK, w.Code)
 		assert.Equal(t, "application/json", w.Header().Get("Content-Type"))
-
-		fmt.Fprintln(os.Stdout, "_____")
-		fmt.Fprintln(os.Stdout, w.Body.String())
-		fmt.Fprintln(os.Stdout, "_____")
-
-		expectedJSON := "{\"results\":[{\"id\":1,\"name\":\"Пельмени\",\"price\":5,\"description\":\"С говядиной\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932687/repos/momos/8dee5a92281746aa887d6f19cf9fdcc7.jpg\"},{\"id\":2,\"name\":\"Хинкали\",\"price\":3.5,\"description\":\"Со свининой\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932687/repos/momos/50b583271fa0409fb3d8ffc5872e99bb.jpg\"},{\"id\":3,\"name\":\"Манты\",\"price\":2.75,\"description\":\"С мясом молодых бычков\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/8b50f76f514a4ccaaacdcb832a1b3a2f.jpg\"},{\"id\":4,\"name\":\"Буузы\",\"price\":4,\"description\":\"С телятиной и луком\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/788c073d83c14b3fa00675306dfb32b5.jpg\"},{\"id\":5,\"name\":\"Цзяоцзы\",\"price\":7.25,\"description\":\"С говядиной и свининой\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/32cc88a33c3243a6a8838c034878c564.jpg\"},{\"id\":6,\"name\":\"Гедза\",\"price\":3.5,\"description\":\"С соевым мясом\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/7685ad7e9e634a58a4c29120ac5a5ee1.jpg\"},{\"id\":7,\"name\":\"Дим-самы\",\"price\":2.65,\"description\":\"С уткой\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/4bdaeab0ee1842dc888d87d4a435afdd.jpg\"},{\"id\":8,\"name\":\"Момо\",\"price\":5,\"description\":\"С бараниной\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/f64dcea998e34278a0006e0a2b104710.jpg\"},{\"id\":9,\"name\":\"Вонтоны\",\"price\":4.1,\"description\":\"С креветками\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932687/repos/momos/8dee5a92281746aa887d6f19cf9fdcc7.jpg\"},{\"id\":10,\"name\":\"Баоцзы\",\"price\":4.2,\"description\":\"С капустой\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932687/repos/momos/50b583271fa0409fb3d8ffc5872e99bb.jpg\"},{\"id\":11,\"name\":\"Кундюмы\",\"price\":5.45,\"description\":\"С грибами\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/8b50f76f514a4ccaaacdcb832a1b3a2f.jpg\"},{\"id\":12,\"name\":\"Курзе\",\"price\":3.25,\"description\":\"С крабом\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/788c073d83c14b3fa00675306dfb32b5.jpg\"},{\"id\":13,\"name\":\"Бораки\",\"price\":4,\"description\":\"С говядиной и бараниной\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/7685ad7e9e634a58a4c29120ac5a5ee1.jpg\"},{\"id\":14,\"name\":\"Равиоли\",\"price\":2.9,\"description\":\"С рикоттой\",\"image\":\"https://res.cloudinary.com/sugrobov/image/upload/v1651932686/repos/momos/4bdaeab0ee1842dc888d87d4a435afdd.jpg\"}]}\n"
-
-		assert.NoError(t, err)
-		assert.JSONEq(t, string(expectedJSON), w.Body.String())
+	
+		expectedJSON := `
+		{
+			"results": [
+				{
+					"id": 1,
+					"name": "Пельмени",
+					"price": 5,
+					"description": "С говядиной",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/1.jpg"
+				},
+				{
+					"id": 2,
+					"name": "Хинкали",
+					"price": 3.5,
+					"description": "Со свининой",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/2.jpg"
+				},
+				{
+					"id": 3,
+					"name": "Манты",
+					"price": 2.75,
+					"description": "С мясом молодых бычков",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/3.jpg"
+				},
+				{
+					"id": 4,
+					"name": "Буузы",
+					"price": 4,
+					"description": "С телятиной и луком",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/4.jpg"
+				},
+				{
+					"id": 5,
+					"name": "Цзяоцзы",
+					"price": 7.25,
+					"description": "С говядиной и свининой",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/5.jpg"
+				},
+				{
+					"id": 6,
+					"name": "Гедза",
+					"price": 3.5,
+					"description": "С соевым мясом",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/6.jpg"
+				},
+				{
+					"id": 7,
+					"name": "Дим-самы",
+					"price": 2.65,
+					"description": "С уткой",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/7.jpg"
+				},
+				{
+					"id": 8,
+					"name": "Момо",
+					"price": 5,
+					"description": "С бараниной",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/8.jpg"
+				},
+				{
+					"id": 9,
+					"name": "Вонтоны",
+					"price": 4.1,
+					"description": "С креветками",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/9.jpg"
+				},
+				{
+					"id": 10,
+					"name": "Баоцзы",
+					"price": 4.2,
+					"description": "С капустой",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/10.jpg"
+				},
+				{
+					"id": 11,
+					"name": "Кундюмы",
+					"price": 5.45,
+					"description": "С грибами",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/11.jpg"
+				},
+				{
+					"id": 12,
+					"name": "Курзе",
+					"price": 3.25,
+					"description": "С крабом",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/12.jpg"
+				},
+				{
+					"id": 13,
+					"name": "Бораки",
+					"price": 4,
+					"description": "С говядиной и бараниной",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/13.jpg"
+				},
+				{
+					"id": 14,
+					"name": "Равиоли",
+					"price": 2.9,
+					"description": "С рикоттой",
+					"image": "https://storage.yandexcloud.net/pictures-momo-store-std-032-63/14.jpg"
+				}
+			]
+		}`
+	
+		t.Log("Response body:", w.Body.String())
+		assert.JSONEq(t, expectedJSON, w.Body.String())
 	})
 
 	t.Run("healthcheck", func(t *testing.T) {
